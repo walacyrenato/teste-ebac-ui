741fbab24c483b479239df1267cd02b84fceffff
Walacy renato <walacyrenatosousapereira@gmail.com>
2025-05-09 10:49:26 -0300

    Alterações no teste de pré-cadastro

--- cypress/e2e/Pré-cadastro.spec.cy.js
+++ cypress/e2e/Pré-cadastro.spec.cy.js
@@ -7,16 +7,19 @@ describe('Funcionalidade pré-cadastro', () => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
   });
   
-  
 
-  it('Deve criar um pré-cadastro com sucesso', () => {
-     cy.get('#reg_email').type(faker.internet.email())
+  it('Deve completar o pré-cadastro com sucesso', () => {
+    let nomeFaker = faker.name.firstName()
+    let sobrenomeFaker = faker.name.lastName()
+    let emailFaker = faker.internet.email(nomeFaker)
+     
+     cy.get('#reg_email').type(emailFaker)
      cy.get('#reg_password').type('@teste@teste$25')
      cy.get(':nth-child(4) > .button').click()
 
      cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-     cy.get('#account_first_name').type(faker.name.firstName())
-     cy.get('#account_last_name').type(faker.name.lastName())
+     cy.get('#account_first_name').type(nomeFaker)
+     cy.get('#account_last_name').type(sobrenomeFaker)
      cy.get('.woocommerce-Button').click()
 
      cy.get('.woocommerce-message').should('contain','Detalhes da conta modificados com sucesso.')
